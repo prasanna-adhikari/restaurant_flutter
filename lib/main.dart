49b2cdd2eb4a42559abc8c61e7717e012f249b60
prasanna-adhikari <prasannaadhikari022@gmail.com>
2021-05-11 19:55:37 +0545

    drawer designed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:restaurant/pages/home.dart';
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     initialRoute: '/home',
     routes: {
       '/home':(context)=> Home(),
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,73 +1,145 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
-final List<String> imgList = [
-  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
-];
-final List<Widget> imageSliders = imgList.map((item) => Container(
-  child: Container(
-    margin: EdgeInsets.all(0),
-    child: ClipRRect(
-        borderRadius: BorderRadius.all(Radius.circular(0.0)),
-        child: Stack(
-          children: <Widget>[
-            Image.network(item, fit: BoxFit.cover, width: 1000.0),
-            Positioned(
-              bottom: 0.0,
-              left: 0.0,
-              right: 0.0,
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      Color.fromARGB(0, 0, 0, 0),
-                      Color.fromARGB(0, 0, 0, 0)
-                    ],
-                    begin: Alignment.bottomCenter,
-                    end: Alignment.topCenter,
-                  ),
-                ),
-                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                child: Text(
-                  'No. ${imgList.indexOf(item)} image',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        )
-    ),
-  ),
-)).toList();
+
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Complicated image slider demo')),
-      body: Container(
-          child: Column(children: <Widget>[
-            CarouselSlider(
-              options: CarouselOptions(
-                autoPlay: true,
-                aspectRatio: 2.0,
-                enlargeCenterPage: true,
-              ),
-              items: imageSliders,
+      appBar: AppBar(
+          title: Text('RoboRes'),
+        backgroundColor: Colors.deepOrangeAccent,
+        actions: [
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: (){}
             ),
-          ],)
+            IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: (){}
+            ),
+
+        ],
+        elevation: 0.0,
       ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+                accountName: Text('Prasanna'),
+                accountEmail: Text('parsanna@gmail.com'),
+
+              currentAccountPicture: GestureDetector(
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  backgroundImage: AssetImage('images/Chris.jpg')
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.deepOrangeAccent,
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              splashColor: Colors.grey[800],
+              child: ListTile(
+                title: Text('Home'),
+                leading: Icon(Icons.home_sharp),
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              splashColor: Colors.grey[800],
+              child: ListTile(
+                title: Text('Categories'),
+                leading: Icon(Icons.dashboard_sharp),
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              splashColor: Colors.grey[800],
+              child: ListTile(
+                title: Text('My Orders'),
+                leading: Icon(Icons.shopping_basket_sharp),
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              splashColor: Colors.grey[800],
+              child: ListTile(
+                title: Text('Favourites'),
+                leading: Icon(Icons.favorite),
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              splashColor: Colors.grey[800],
+              child: ListTile(
+                title: Text('My Account'),
+                leading: Icon(Icons.person_sharp),
+              ),
+            ),
+            Divider(),
+            InkWell(
+              onTap: (){},
+              splashColor: Colors.grey[800],
+              child: ListTile(
+                title: Text('Setting'),
+                leading: Icon(Icons.settings),
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              splashColor: Colors.grey[800],
+              child: ListTile(
+                title: Text('About'),
+                leading: Icon(Icons.help),
+              ),
+            ),
+            Divider(),
+            InkWell(
+              onTap: (){},
+              splashColor: Colors.grey[800],
+              child: ListTile(
+                title: Text('Log out', style: TextStyle(color: Colors.grey[800]),),
+
+                leading: Icon(Icons.logout),
+              ),
+            ),
+
+          ],
+        ),
+      ),
+      body: Container(
+          child: SizedBox(
+            height: 220.0,
+            width: double.infinity,
+            child: Carousel(
+              boxFit: BoxFit.cover,
+              autoplay: true,
+              animationCurve: Curves.fastOutSlowIn,
+              animationDuration: Duration(milliseconds: 1000),
+              dotSize: 6.0,
+              dotIncreasedColor: Color(0xFFFF335C),
+              dotBgColor: Colors.transparent,
+              dotPosition: DotPosition.bottomCenter,
+              dotVerticalPadding: 10.0,
+              showIndicator: true,
+              indicatorBgPadding: 7.0,
+              images: [
+                AssetImage('images/slide1.jpg'),
+                AssetImage('images/slide2.jpg'),
+                AssetImage('images/slide3.jpg'),
+              ],
+            ),
+          ),
+        )
+
+
     );
   }
 }
